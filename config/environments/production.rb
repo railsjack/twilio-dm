d653e7757b57e8f73129c4ef26251db7a8f7e09b
Web Dev <jinnahrae@gmail.com>
2019-02-04 02:06:23 -0800

    Added sendgrid info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,8 +103,10 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => 'jinnahrae',
+      :password       => 'akffjro123',
+      #:user_name      => ENV['SENDGRID_USERNAME'],
+      #:password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
